3cdab657716596c6dc47ac39ffdffc46f9db5b96
llama-herder <156974895+llama-herder@users.noreply.github.com>
2024-04-20 16:10:40 -0500

    Update file.py

--- file.py
+++ file.py
@@ -7,7 +7,26 @@ cap=cv2.VideoCapture(0)
 while (1):
     _,frame=cap.read()
 
+    #below is variables of each color, but not only that color
+    red=np.matrix(frame[:,:,2]) #B G R
+    green=np.matrix(frame[:,:,1])
+    blue=np.matrix(frame[:,:,0])
+
+    #separated red image layer
+    red_only=np.int16(red)-np.int16(green)-np.int16(blue)
+    
+    #limits matrix to standard color range
+    red_only[red_only<0]=0
+    red_only[red_only>255]=255
+
+    #sets matrix back to image quality
+    red_only=np.uint8(red_only)
+
+    #displays windows
     cv2.imshow('Frame', frame)
+    cv2.imshow('Red Only', red_only)
+
+
 
     k=cv2.waitKey(5)
     #will wait until key press (5>?)
